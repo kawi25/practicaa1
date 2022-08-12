42121045a748fcc99e44755211c901cde2d3fd94
Lab Computo <Computosala42022@ugb.edu.sv>
2022-08-12 11:06:30 -0600

    ACTUALIZACION

--- practicaa1/Program.cs
+++ practicaa1/Program.cs
@@ -6,7 +6,25 @@ namespace practicaa1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("ingrese su promdio");
+            double nota = double.Parse(Console.ReadLine());
+            if (nota >= 0 && nota <= 10)
+                if (nota >= 6)
+
+                    Console.WriteLine("aprobado");
+
+                else if (nota >= 5)
+
+                    Console.WriteLine("repiosicion");
+
+                else
+                    Console.WriteLine("reprovado");
+
+            else
+
+                Console.WriteLine("la nota no es corecta");
+
+
         }
     }
 }
